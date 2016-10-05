468e5a53e0cdd6d267dab1c94735d06d6177efef
ArnaudPl <arnaud.plumez@he-arc.ch>
2016-10-05 14:52:54 -0700

    Add Account show basics, need to let the user edit the account (name).

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -9,6 +9,11 @@ class AccountsController < ApplicationController
     @accounts = Account.all
   end
 
+  def show
+    @account = Account.find(params[:id])
+    @balance = calculateAccountCurrentBalance(@account.id)
+  end
+
   def calculateAccountCurrentBalance(account_id)
       withdraws = Transaction.all.where(withdraw: true).where(account_id: account_id).sum(:amount)
       deposits = Transaction.all.where(withdraw: false).where(account_id: account_id).sum(:amount)
@@ -20,4 +25,23 @@ class AccountsController < ApplicationController
       end
   end
   helper_method :calculateAccountCurrentBalance
+
+  def printAccountLatestTransactions(account_id)
+    html = ""
+    transactions = Transaction.all.where(account_id: account_id).order(:created_at)
+    transactions.each do |transaction|
+      if transaction.withdraw
+        html += '<li class="collection-item green row white-text"><span class="col s1 center">+</span><span class="col s5 truncate">' + transaction.label + '</span><span class="col s3">' + transaction.amount.to_s + 'CHF</span><span class="col s2">' + transaction.created_at.strftime("%e %b") + '</span></li>'
+      else
+        html += '<li class="collection-item red row white-text"><span class="col s1 center">-</span><span class="col s5 truncate">' + transaction.label + '</span><span class="col s3">' + transaction.amount.to_s + 'CHF</span><span class="col s2">' + transaction.created_at.strftime("%e %b") + '</span></li>'
+      end
+    end
+    if html == ""
+      html = '<li class="collection-item center grey lighten-3">' + I18n.t('.no-transactions') + '</li>'
+    else
+      html = '<li class="collection-item grey lighten-2 row"><span class="col s1">+/-</span><span class="col s5 truncate">' + I18n.t('.label') + '</span><span class="col s3">' + I18n.t('.amount') + '</span><span class="col s2">' + I18n.t('.date') + '</span></li>' + html
+    end
+    html
+  end
+  helper_method :printAccountLatestTransactions
 end
